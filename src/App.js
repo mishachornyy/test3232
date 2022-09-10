3fbb5f4d5e10b2c5878cd8e009b804c031b57863
mishachornyy <85844519+mishachornyy@users.noreply.github.com>
2022-09-09 18:50:40 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Menu from './component/Menu'
+import './App.css';
+import Footer from "./component/Footer";
+import Head from "./component/Head";
+
+function App() {
+  return (
+    <> <Menu />
+      <Head />
+       <Footer />
+    </>
+  );
+
+}
+
+export default App;
+
+
